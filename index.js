a52210c44182c3b64dcf310106ccba2edc5eae73
kwamamissah@gmail.com <kwamamissah@gmail.com>
2018-09-10 01:49:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,6 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(facts) {
  
-  /*var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];*/
-  
   var array = [];
   
    var i = 0;
